7090662b64d582faecc7bb5c74c5e558e17c6548
Pinar Ayaz <pinarayaz123@gmail.com>
2018-11-02 10:37:06 +0100

    solved dp3 ex3

--- exercises/DP3/src/ch/epfl/sweng/dp3/ex3/IntegerBox.java
+++ exercises/DP3/src/ch/epfl/sweng/dp3/ex3/IntegerBox.java
@@ -1,6 +1,9 @@
 package ch.epfl.sweng.dp3.ex3;
 
-public class IntegerBox {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class IntegerBox implements Iterable<Integer>{
   private int[] box;
 
   public IntegerBox(int... numbers) {
@@ -19,9 +22,33 @@ public class IntegerBox {
       newBox[i] = box[i];
     }
     newBox[box.length] = number;
+    box = newBox;
   }
 
   public int[] getIntegers() {
     return box;
+  }
+
+  public Iterator iterator(){
+    return new Iterator<Integer>(){
+      private int current = 0;
+      private int length = box.length;
+
+      @Override
+      public boolean hasNext(){
+        return current < length;
+      }
+
+      @Override
+      public Integer next(){
+        if(hasNext()){
+          return box[current++];
+        }
+        else{
+          throw new NoSuchElementException();
+        }
+      }
+
+    };
   }
 }
